18684ec944a23700ed3632c4bbdfba82958a91f3
Jorge Bucaran <mail@jorgebucaran.com>
2016-04-09 11:57:04 +0900

    Remove usage help test

--- test/other.fish
+++ test/other.fish
@@ -10,9 +10,3 @@ test "$TESTNAME - no data prints nothing and exits with a 1"
         echo $status
         )
 end
-
-test "$TESTNAME - show help information"
-    "Usage: shark [--line] [--field[=<format>]] [--record[=<format>]] [--max=<n>] [--min=<n>] [--delim=<regex>] [--help] -n --line Process each line as a record -f --field=<format> Set field output format -r --record=<format> Set record output format -m --min=<number> Set minimum numeric value -M --max=<number> Set maximum numeric value -d --delim=<regex> Use <regex> as number separator --null=<string> Use <string> to display null values -h --help Show this help" = (
-        shark -h | xargs
-        )
-end
