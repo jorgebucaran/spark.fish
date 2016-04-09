72fa23a5c1c702827ecef8adc93e92babf32b710
Jorge Bucaran <mail@jorgebucaran.com>
2016-04-09 11:57:28 +0900

    Remove awk file and embed it in shark.fish for simplicity.

--- functions/shark.fish
+++ functions/shark.fish
@@ -1,4 +1,4 @@
-function shark -d "Sparkline Generator"
+function shark --description 'Sparkline generator'
     set -l field "%s"
     set -l record "\n"
     set -l delim "[, ]"
@@ -13,6 +13,7 @@ function shark -d "Sparkline Generator"
         switch $1
             case n line
                 set line 1
+
                 switch "$2"
                     case 0 false
                         set line 0
@@ -37,17 +38,17 @@ function shark -d "Sparkline Generator"
                 set delim "$2"
 
             case h help
-                printf "Usage: shark [--line] [--field[=<format>]] [--record[=<format>]]  \n"
-                printf "             [--max=<n>] [--min=<n>] [--delim=<regex>] [--help] \n\n"
+                printf "Usage: shark [--line] [--field[=<format>]] [--record[=<format>]]\n"
+                printf "             [--max=<n>] [--min=<n>] [--delim=<regex>] [--help]\n\n"
 
-                printf "               -n --line  Process each line as a record         \n"
-                printf "     -f --field=<format>  Set field output format               \n"
-                printf "    -r --record=<format>  Set record output format              \n"
-                printf "       -m --min=<number>  Set minimum numeric value             \n"
-                printf "       -M --max=<number>  Set maximum numeric value             \n"
-                printf "      -d --delim=<regex>  Use <regex> as number separator       \n"
-                printf "         --null=<string>  Use <string> to display null values   \n"
-                printf "               -h --help  Show this help                        \n"
+                printf "     -n --line              Process each line as a record         \n"
+                printf "     -f --field=<format>    Set field output format               \n"
+                printf "     -r --record=<format>   Set record output format              \n"
+                printf "     -m --min=<number>      Set minimum numeric value             \n"
+                printf "     -M --max=<number>      Set maximum numeric value             \n"
+                printf "     -d --delim=<regex>     Use <regex> as number separator       \n"
+                printf "        --null=<string>     Use <string> to display null values   \n"
+                printf "     -h --help              Show this help\n"
                 return
 
             case \*
@@ -57,12 +58,70 @@ function shark -d "Sparkline Generator"
         end
     end
 
-    awk -v FS="$delim"      \
-        -v line=$line       \
-        -v ORS="$record"    \
-        -v OFS="$field"     \
-        -v null="$null"     \
-        -v min="$min"       \
-        -v max="$max"       \
-        -f $shark_path/shark.awk
+    awk -v FS="$delim" -v line=$line -v ORS="$record" -v OFS="$field" -v null="$null" -v min="$min" -v max="$max" '
+            BEGIN {
+                tickLength = split("▁ ▂ ▃ ▄ ▅ ▆ ▇ █", ticks, " ")
+
+                hasMax = max > 0
+                hasMin = min >= 0
+
+                max = min > max ? min : max
+            }
+
+            !/^ *$/ {
+                if (line) {
+                    fieldIndex = 0
+                    min = hasMin ? min : -1
+                    max = hasMax ? max : 0
+                }
+
+                for (n = 1; n <= NF; n++) {
+                    $n = buffer[++fieldIndex] = ($n ~ /^[0-9]*\.?[0-9]+$/) ? $n : ""
+
+                    if (!hasMax) max = ($n > max) ? $n : max
+                    if (!hasMin) min = (min < 0 || $n < min) ? ($n == "" ? min : $n) : min
+                    if (hasMax && hasMin) plot($n)
+                }
+
+                if (line) {
+                    if (!(hasMax && hasMin)) {
+                        plotBuffer(fieldIndex)
+                    }
+
+                    printf(ORS, max, min, fieldIndex)
+                }
+            }
+
+            END {
+                if (line || !fieldIndex) exit !fieldIndex
+                if (!(hasMax && hasMin)) plotBuffer(fieldIndex)
+
+                printf(ORS, max, min, fieldIndex)
+            }
+
+            function plot(value, tickIndex) {
+                tickToken = null
+
+                if (value >= 0) {
+
+                    tickIndex = int((value / (!max ? 1 : max) * tickLength) - 1) + 1
+
+                    tickIndex = (tickIndex <= 0) ? 1 : tickIndex
+                    tickIndex = (tickIndex > 8 || max == value) ? 8 : tickIndex
+                    tickIndex = (min == max) ? 5 : tickIndex
+                    tickIndex = (max == 0) ? 1 : tickIndex
+
+                    tickToken = ticks[tickIndex]
+                }
+
+                printf(OFS, tickToken, value)
+            }
+
+            function plotBuffer(fieldIndex) {
+                for (i = 1; i <= fieldIndex; i++) {
+                    plot(buffer[i])
+                }
+            }
+
+        '
 end
